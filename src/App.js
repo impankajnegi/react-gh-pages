54cd8fe029c6663fa6ff37b3c4235a609568220f
Pankaj Negi <pankajnegi@integramicro.com>
2020-07-24 01:18:06 +0530

    Pushing code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import PnContainer from './Components/Container'; 
+import Button from '@material-ui/core/Button';
+import SignUp from './Components/SignUp'
+import SignIn from './Components/SignIn'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       {/* <SignUp></SignUp> */}
+       <SignIn></SignIn>
     </div>
+ 
   );
 }
 
---
+++ src/Components/Container.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+export default function PnContainer() {
+  return (
+    <React.Fragment>
+ 
+    </React.Fragment>
+  );
+}
